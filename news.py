2c6d97ba634abf1ee40e8c326059d8a7d2ab0250
Suraj3455 <surajthorat415@gmail.com>
2025-07-23 11:13:43 +0530

    Update news.py

--- news.py
+++ news.py
@@ -63,17 +63,22 @@ def send_alert_email(user_email):
 # ------------------------- API -------------------------
 api_key = "380a2141d0b34d91931aa5a856a37d6f"
 
+from datetime import datetime
+
 def fetch_news(category=None, keyword=None):
     base_url = "https://newsapi.org/v2/"
+    today = datetime.today().strftime('%Y-%m-%d')
+
     if keyword:
-        url = f"{base_url}everything?apiKey={api_key}&q={keyword}&language=en&sortBy=publishedAt"
+        url = f"{base_url}everything?apiKey={api_key}&q={keyword}&from={today}&sortBy=publishedAt&language=en"
     else:
         url = f"{base_url}top-headlines?apiKey={api_key}&language=en"
         if category:
             url += f"&category={category}"
-    response = requests.get(url)
-    return response.json().get("articles", [])
 
+    response = requests.get(url)
+    print(response.status_code, response.text)  # Optional: debug
+    return response.json().get("articles", [])
 # ------------------------- NLP -------------------------
 def analyze_sentiment_all(text):
     blob_polarity = TextBlob(text).sentiment.polarity
